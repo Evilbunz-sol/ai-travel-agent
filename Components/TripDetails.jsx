c639a1da7fb763cf0eb6b37b60be47003fdf1a9e
Evilbunz <jameel.altamash@gmail.com>
2024-05-15 21:10:55 -0400

    up

--- Components/TripDetails.jsx
+++ Components/TripDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {useNavigate, useLocation} from "react-router-dom"
-import "../styles.css"
+import "../index.css"
+
 
 export default function TripDetails() {
     const navigate = useNavigate()
